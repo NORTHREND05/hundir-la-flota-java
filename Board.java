800f87341d715dd7aebcfb90e329ac196ffd5f47
Joel Campo <joelcampo@gmail.com>
2026-01-31 17:02:21 +0100

    Implementa colocación aleatoria de barcos

--- Board.java
+++ Board.java
@@ -1,13 +1,18 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Board {
     private final int size;
     private final char[][] grid;
+    private final Random rnd = new Random();
+
+    private int totalShips;
 
     public Board(int size) {
         this.size = size;
         this.grid = new char[size][size];
         fillWater();
+        this.totalShips = 0;
     }
 
     private void fillWater() {
@@ -20,8 +25,26 @@ public class Board {
         return size;
     }
 
+    public int getTotalShips() {
+        return totalShips;
+    }
+
     public char getCell(int row, int col) {
         return grid[row][col];
+    }
+
+    public void placeShipsRandom(int ships) {
+        int placed = 0;
+        while (placed < ships) {
+            int r = rnd.nextInt(size);
+            int c = rnd.nextInt(size);
+
+            if (grid[r][c] == '~') {
+                grid[r][c] = 'B';
+                placed++;
+            }
+        }
+        totalShips = ships;
     }
 
     // Para mostrar el tablero (de momento sin ocultar nada)
